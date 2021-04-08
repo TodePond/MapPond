65dc5173367ea7e66f13bf6442c4064d54dd8f8d
Luke Wilson <l2wilson94@gmail.com>
2021-04-08 09:50:04 +0100

    mothertode reads save file

--- build/build-embed.js
+++ build/build-embed.js
@@ -2,17 +2,59 @@ const stage = Stage.make()
 const {canvas, context} = stage
 
 const camera = {x: 0, y: 0, scale: 1}
-
 const entities = new Map()
-const createEntity = (source, {x = 0, y = 0, scale = 0} = {}) => {
-    const image = getImage(source)
-    const id = entities.size
-    const entity = {id, source, image, x, y, scale}
-    entities.set(id, entity)
+const freeIds = new Set()
+const imageCache = new Map()
+
+// Make an entity AND THEN place it on the map
+const createEntity = (...args) => {
+    const entity = makeEntity(...args)
+    registerEntity(entity)
     return entity
 }
 
-const imageCache = new Map()
+// Get an entity id that is free to use (note: remember to remove it from the freeIds list if you use it)
+const getNewId = () => {
+    if (freeIds.size > 0) {
+		return freeIds.values().next().value
+	}
+    else return entities.size
+}
+
+// Place an entity on the map
+const registerEntity = (entity) => {
+    const id = getNewId()
+	loadEntity(entity, id)
+    return id
+}
+
+// Place an entity on the map in a specific id
+const loadEntity = (entity, id) => {
+	entity.id = id
+	entities.set(id, entity)
+	freeIds.delete(id)
+}
+
+// Remove an entity from the map
+const unregisterEntity = (id) => {
+    freeIds.add(id)
+    entities.delete(id)
+}
+
+// Remove all entities
+const unregisterAllEntities = () => {
+	freeIds.clear()
+	entities.clear()
+}
+
+// Make an entity object
+const makeEntity = (source, {x = 0, y = 0, scale = 1} = {}) => {
+    const image = getImage(source)
+    const entity = {id: undefined, source, image, x, y, scale}
+    return entity
+}
+
+// Get an image element (make one if needed)
 const getImage = (source) => {
     const cachedImage = imageCache.get(source)
     if (cachedImage !== undefined) return cachedImage
@@ -28,8 +70,9 @@ on.load(() => {
     canvas.style["background-color"] = "rgb(45, 56, 77)"
     trigger("resize")
 
-    const plane = createEntity("Plane.png").d
-    plane.scale = 0.5
+    const plane = createEntity("Plane.png", {x: 100, scale: 0.5}).d
+    
+    const plane2 = createEntity("Plane.png", {x: 50}).d
     
 })
 
@@ -60,20 +103,42 @@ stage.draw = () => {
     }
 }
 
-const saveEntity = (entity) => {
-    let str = ""
-    for (const key in entity) {
-        str += key + ":" + entity[key]
-    }
-    return str
-}
-
+// Save the map state to a string
 const save = () => {
     const lines = []
-    lines.push("camera:" + camera.values().join(","))
-    lines.push("entities:")
+    lines.push(`camera:x=${camera.x},y=${camera.y},scale=${camera.scale}`)
+    lines.push(`entities:`)
     for (const entity of entities.values()) {
-        lines.push(`id:${entity.id},source:${entity.source},x:${entity.x},y:${entity.y},scale:${entity.scale}`)
+        lines.push(`id=${entity.id},source=${entity.source},x=${entity.x},y=${entity.y},scale=${entity.scale}`)
     }
     return lines.join(`\n`)
+}
+
+const Load = MotherTode(`
+	:: Camera "\n" Entities
+	Camera (
+		:: "camera:x=" Number ",y=" Number ",scale=" Number
+		>> ([c, x, _1, y, _2, scale]) => {
+			camera.x = x.output
+			camera.y = y.output
+			camera.scale = scale.output
+		}
+	)
+	Entities :: "entities:" { Entity }
+	Entity (
+		:: "\nid=" Number ",source=" String ",x=" Number ",y=" Number ",scale=" Number
+		>> ([_1, id, _2, source, _3, x, _4, y, _5, scale]) => {
+			const entity = makeEntity(source.output, {x: x.output, y: y.output, scale: scale.output})
+			loadEntity(entity, id.output)
+		}
+	)
+	String :: /[^,]/+
+	Number :: /[0-9.]/+ >> (n) => n.output.as(Number)
+`)
+
+// Load a map state
+const load = (save) => {
+	MotherTode.Term.resetCache()
+	unregisterAllEntities()
+	return Load(save)
 }
